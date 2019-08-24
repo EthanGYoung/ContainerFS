68d98c147bca9e42d2707985f66c240eab8dddb4
E4242 <E4242@node.bloomfilter.gvisoranalysis-pg0.wisc.cloudlab.us>
2019-08-24 13:34:59 -0500

    Added script to install go

---
+++ imgGen/init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get -y upgrade
+
+#https://golang.org/dl/
+wget https://dl.google.com/go/go1.12.9.linux-amd64.tar.gz
+sudo tar -xvf go1.12.9.linux-amd64.tar.gz
+sudo mv go /usr/local
+
+# Set up environment
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/ContainerFS/ContainerFS
+
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
