164484f7cd1bb478ac53a6d69104b6172af6abe6
E4242 <E4242@node.bloomfilter.gvisoranalysis-pg0.wisc.cloudlab.us>
2019-08-25 08:34:31 -0500

    Added bloom filter struct

--- imgGen/src/filter/bloom.go
+++ imgGen/src/filter/bloom.go
@@ -1,6 +1,7 @@
 package filter
 
 import (
+	"bitarray"
 )
 
 // BloomFilter is a struct for creating a Bloom filter for an image file. A
@@ -8,8 +9,15 @@ import (
 // in the image file or is "maybe" in the file with a certain probability. 
 // This struct implements the Filter interface.
 type BloomFilter struct {
-	// TODO:
-	// NumHashFuncs = k -> Calculated from equation
-	// Num Elements -> Set in initialization
-	// Num Bits -> Calculated from equation
+	// NumHashes represents the number of hash functions for this bloom filter
+	NumHashes int64
+
+	// NumElem represents the number of elements in this filter
+	NumElem int64
+
+	// FilterSize represents the number of bits in this filter
+	FilterSize int64
+
+	// FilterArray is the array of bits that implements a bloom filter
+	FilterArray bitarray
 }
