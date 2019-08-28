d224352a9550c3264016afdd1938df578c02cb70
E4242 <E4242@node.bloomfilter.gvisoranalysis-pg0.wisc.cloudlab.us>
2019-08-28 11:06:52 -0500

    Able to run imgs in ContainerFS

--- imgGen/init.sh
+++ imgGen/init.sh
@@ -8,6 +8,15 @@ wget https://dl.google.com/go/go1.12.9.linux-amd64.tar.gz
 sudo tar -xvf go1.12.9.linux-amd64.tar.gz
 sudo mv go /usr/local
 
+
+sudo add-apt-repository ppa:jonathonf/python-3.6
+sudo apt-get update
+sudo apt-get install python3.6
+
+# Install pip and install docker
+sudo apt-get -y install python3-pip
+python3.6 -m pip install docker
+
 # Set up environment
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/ContainerFS/imgGen
